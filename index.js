ce191890a966b460218b163b0a7fb9fc657b97a5
adjash <adam.hardie13@gmail.com>
2026-04-23 19:06:23 +0100

    mega, mega basic login and register routes

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.post("/login", (req, res) => {
+  try {
+    const { username, password } = req.body;
+    if (!username) res.status(401).send("Please enter a username");
+    if (!password) res.status(401).send("Please enter a password");
+
+    if (username === "user" && password === "password") {
+      res.status(200).send("logged in!");
+      return;
+    } else {
+      res.status(401).send("incorrect details");
+      return;
+    }
+  } catch (err) {
+    res.status(403).send("something went wrong");
+    return;
+  }
+});
+
+app.post("/register", (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    if (!username) return res.status(401).send("Please enter a username");
+    if (!email) return res.status(401).send("Please enter an email");
+    if (!password) return res.status(401).send("Please enter a password");
+
+    console.log(username, email, password);
+    res.status(200).send("registered");
+    return;
+  } catch (err) {
+    console.log(err);
+    return res.status(403).send("something went wrong");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
