78154a3af73b6bcdb7461ac058cff0eaa6b3be28
adjash <adam.hardie13@gmail.com>
2026-05-06 21:08:55 +0100

    base route with some database login/register and a super basic cookie setup which doesn't work the way I need it to yet

---
+++ src/index.js
@@ -0,0 +1,93 @@
+import express from "express";
+import session from "express-session";
+import { v4 as uuidv4 } from "uuid";
+import path from "path";
+import { fileURLToPath } from "url";
+import { databaseInstance } from "./db/db.js";
+
+const app = express();
+const port = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//JSON middleware parser, lets us parse JSON from req.body
+app.use(express.json());
+
+//session cookie setup
+//TODO: figure out how to make this only apply to certain routes
+app.use(
+  session({
+    name: "session-cookie",
+    resave: true,
+    saveUninitialized: true,
+    genid: function (req) {
+      return uuidv4();
+    },
+    secret: "keyboard cat",
+    cookie: function (req) {
+      var match = req.url.match(/^\/([^/]+)/);
+      return {
+        path: match ? "/" + match[1] : "/",
+        httpOnly: true,
+        secure: req.secure || false,
+        maxAge: 60000,
+      };
+    },
+  }),
+);
+
+//database setup
+const db = new databaseInstance();
+//for testing while we're using sqlite, just call setup as it'll make sure the tables are setup
+db.setup();
+
+//basic index route for testing
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+//login route
+//makes database function call to check if the provided data is valid
+//I'm starting to think it might make more sense to have the logic to check if the password is valid inside the route, instead of at the database level.
+app.post("/login", (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email) return res.status(400).send("Please enter a email");
+    if (!password) return res.status(400).send("Please enter a password");
+
+    const loginOperation = db.loginUser(email, password);
+    return res
+      .status(loginOperation.status)
+      .json({ message: loginOperation.message });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("something went wrong");
+    return;
+  }
+});
+
+//register user route
+//makes database function call to insert a user with the provided data
+//Might make more sense to have it return a 'inserted', or something similar to indicate the operation happened successfully.
+app.post("/register", (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    if (!username) return res.status(400).send("Please enter a username");
+    if (!email) return res.status(400).send("Please enter an email");
+    if (!password) return res.status(400).send("Please enter a password");
+
+    console.log(username, email, password);
+    const registerOperation = db.insertUser(username, email, password);
+    return res
+      .status(registerOperation.status)
+      .json({ message: registerOperation.message });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("something went wrong");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
